ba292acb97cb44ce93bd6d9121094885cf407539
Nevez06 <joaoantonioneves487@gmail.com>
2026-03-09 17:30:06 -0300

    Criando getter e setters

--- devops/src/main/java/br/com/devops/devops/entity/Curso.java
+++ devops/src/main/java/br/com/devops/devops/entity/Curso.java
@@ -1,5 +1,4 @@
 package br.com.devops.devops.entity;
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,30 +14,18 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Aluno {
-  
-
+public class Curso {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer idAluno;
+    private Integer idCurso;
 
     @Column(nullable = false, length = 40)
-    private String nomeAluno;
+    private String nomeCurso;
     
-    @Column(length = 100)
-    private String emailAluno;
-    
-    @Column(nullable = false, length = 11)
-    private String telefoneAluno;
+    @Column(nullable = false, length = 40)
+    private String periodoCurso;
 
-    @Column(nullable = false, length = 50)
-    private String enderecoAluno;
-
-    @Column(nullable = false, length = 11)
-    private String cpfAluno;
-
-    @Column(nullable = false)
-    private String raAluno;
-
+    @Column(nullable = false, length = 100)
+    private Integer cargahorariaCurso;
 
 }
